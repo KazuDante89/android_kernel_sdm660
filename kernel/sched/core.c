4c47ecc639800ef4a10b91ad252375afcc8fcd41
Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
2016-11-02 19:50:29 +0900

    sched/core: Fix oops in sched_show_task()
    When CONFIG_THREAD_INFO_IN_TASK=y, it is possible that an exited thread
    remains in the task list after its stack pointer was already set to NULL.
    
    Therefore, thread_saved_pc() and stack_not_used() in sched_show_task()
    will trigger NULL pointer dereference if an attempt to dump such thread's
    traces (e.g. SysRq-t, khungtaskd) is made.
    
    Since show_stack() in sched_show_task() calls try_get_task_stack() and
    sched_show_task() is called from interrupt context, calling
    try_get_task_stack() from sched_show_task() will be safe as well.
    
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Acked-by: Andy Lutomirski <luto@kernel.org>
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: bp@alien8.de
    Cc: brgerst@gmail.com
    Cc: jann@thejh.net
    Cc: keescook@chromium.org
    Cc: linux-api@vger.kernel.org
    Cc: tycho.andersen@canonical.com
    Link: http://lkml.kernel.org/r/201611021950.FEJ34368.HFFJOOMLtQOVSF@I-love.SAKURA.ne.jp
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5289,6 +5289,8 @@ void sched_show_task(struct task_struct *p)
 	int ppid;
 	unsigned long state = p->state;
 
+	if (!try_get_task_stack(p))
+		return;
 	if (state)
 		state = __ffs(state) + 1;
 	printk(KERN_INFO "%-15.15s %c", p->comm,
@@ -5309,6 +5311,7 @@ void sched_show_task(struct task_struct *p)
 
 	print_worker_info(KERN_INFO, p);
 	show_stack(p, NULL);
+	put_task_stack(p);
 }
 
 void show_state_filter(unsigned long state_filter)
